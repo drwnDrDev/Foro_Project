e9a38781e1ca6c23c497117d16470c271ddbb3a7
Julian David Ramirez Manrique <jramirezman@unal.edu.co>
2024-06-06 22:14:04 -0500

    retornar array

--- FORO/FORO/urls.py
+++ FORO/FORO/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from categories.views import categories
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('categories/', categories, name='categories'),
+   
 ]
