d55096005143102a0b07d067f1536353c8610e63
Julian David Ramirez Manrique <jramirezman@unal.edu.co>
2024-06-08 21:37:43 -0500

    vistas

--- FORO/FORO/urls.py
+++ FORO/FORO/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from categories.views import categories
-from home.views import home
+from home.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', home),
+    path('contact', contact),
+    path('about', about),
     path('admin/', admin.site.urls),
     path('categories/', categories),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- FORO/home/views.py
+++ FORO/home/views.py
@@ -10,4 +10,11 @@ def home(request):
     else:
         jornada = "pm"
     context={"jornada":jornada}
-    return render(request, loader.get_template(home),context)
+    return render(request,'home/index.html',context)
+
+def contact(request):
+    contex={"title":"Contacto", "message":"Bienvenido a la pagina de contacto", "content":"Si tienes alguna duda o sugerencia, por favor no dudes en contactarnos",
+            "darwin":{"phone": "1234567890","email":"dwd@yahoo.com"}, "carlos":{"phone": "0987654321","email":"fotoalep@gmail.com"}}
+    return render(request,'home/contact.html',contex)
+def about(request):
+    return render(request,'home/about.html')
