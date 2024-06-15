b9198a449611482e25b073ed91fb26fdd7ef5f5f
Foto Aleph <fotoaleph@gmail.com>
2024-06-15 11:51:07 -0500

    posts vista

--- FORO/ForoApp/urls.py
+++ FORO/ForoApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('threads/', views.threads, name='threads'),
-    path('threads/create/', views.threads_create, name='threads_create')
+    path('threads/create/', views.threads_create, name='threads_create'),
+    path('threads/1',views.threads_show, name='threads_show'),
 ]
