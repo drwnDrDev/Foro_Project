f86deb6257e5c3678f138d45c35f20bd08829668
Julian David Ramirez Manrique <jramirezman@unal.edu.co>
2024-06-30 10:09:53 -0500

    estilos bon mostrar/ocultar contraseña

--- FORO/ForoApp/static/js/register.js
+++ FORO/ForoApp/static/js/register.js
@@ -1,6 +1,6 @@
 const pdws =document.getElementById('password')
 const rPdws=document.getElementById('r-password')
-const ver =document.getElementById('ver')
+const ver =document.querySelectorAll('.password')
 
 rPdws.addEventListener('keyup',()=>{
     if(pdws.value===rPdws.value){
@@ -12,14 +12,32 @@ rPdws.addEventListener('keyup',()=>{
         rPdws.classList.add('incorrect')
     }
 })
-ver.addEventListener('mousedown', ()=>{
-        rPdws.type='text'
-        ver.classList.remove('i-unseen')
-        ver.classList.add('i-ver')       
+
+
+ver.forEach((element)=>{
+    let campo = element.childNodes[1].childNodes[1]
+    let mostrar=element.childNodes[1].childNodes[3]  
+    element.addEventListener('mousedown',()=>{
+            campo.type='text'        
+            mostrar.classList.remove('i-unseen')
+            mostrar.classList.add('i-ver')
     })
-    
-ver.addEventListener('mouseup', ()=>{
-        rPdws.type='password'
-        ver.classList.remove('i-ver')
-        ver.classList.add('i-unseen')       
-    })
+    element.addEventListener('mouseup',()=>{
+        campo.type='password'
+        mostrar.classList.remove('i-ver')
+        mostrar.classList.add('i-unseen') 
+    })  
+})
+
+// function show_password(campo){
+//         campo.type='text'
+//         ver.classList.remove('i-ver')
+//         ver.classList.add('i-unseen') 
+//      }
+// ver.addEventListener('mousedown', show_password(e.target))
+
+// ver.addEventListener('mouseup', ()=>{
+//         rPdws.type='password'
+//         ver.classList.remove('i-ver')
+//         ver.classList.add('i-unseen')       
+//     })
